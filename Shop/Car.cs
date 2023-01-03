d5f97d32582e282ac01845f2f7921bc27ad9a63d
Nick Ivanov <110496111+NickIvanov1992@users.noreply.github.com>
2023-01-03 22:57:57 +0400

    Add Views and SearchController MVC

---
+++ Shop/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop
+{
+    public class Car
+    {
+        public int Id { get; }
+        public string Model { get; }
+        public Car(int id, string model)
+        {
+            Model = model;
+            Id = id;
+        }
+
+    }
+}
